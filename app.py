8f2c9ae835195c7d461fffd95322e52ccd3b39e3
reneenoble <renee.noble@hotmail.com>
2024-06-24 12:33:20 +0000

    Formatting and finalising

--- app.py
+++ app.py
@@ -40,37 +40,22 @@ search_client = SearchClient(
 
 
 # PUT YOUR CODE FOR GETTING YOUR AI ANSWER INSIDE THIS FUNCTION
-def get_response(question):
+def get_response(question, message_history=[]):
    
   # 
   # AI CODE GOES IN HERE
   #
 
-  SYSTEM_MESSAGE = "You are an assistant that speaks like a surfer. You can answer questions and provide information. You can also provide sources for your information."
+  return answer, message_history + [{"role": "user", "content": question}]
 
 
-  # What question do you want to ask?
-  question = "What is the capital of France?"
-  search_results = search_client.search(search_text=question)
-  search_summary = " ".join(result["content"] for result in search_results)
 
-  # Create the message history
-  messages=[
-      {"role": "system", "content": SYSTEM_MESSAGE},
-      {"role": "user", "content": question + "\nSources: " + search_summary},
-  ]
-
-  # Get the answer using the GPT model (create 1 answer (n) and use a temperature of 0.7 to set it to be pretty creative/random)
-  response = client.chat.completions.create(model=MODEL_NAME,temperature=0.7,n=1,messages=messages)
-  answer = response.choices[0].message.content
-  print(answer)
-
-  return answer
 
 
 ############################################
 ######## THIS IS THE WEB APP CODE  #########
 ############################################
+
 # Create a flask app
 app = Flask(
   __name__,
@@ -79,9 +64,7 @@ app = Flask(
 )
 
 
-
-
-# create an index route
+# This is the route for the home page (it links to the pages we'll create)
 @app.get('/')
 def index():
   # Return a page that links to these three pages /test-ai, /ask, /chat
@@ -89,43 +72,14 @@ def index():
             <a href="/ask">Ask</a> <br> 
             <a href="/chat">Chat</a>"""
 
-# This is the route that shows the form the user asks a question on
-@app.get('/test-ai')
-def test_ai():
-    # Very basic form that sends a question to the /contextless-message endpoint
-    return """
-    <h1>Ask a question!</h1>
-    <form method="post" action="/test-ai">
-        <textarea name="question" placeholder="Ask a question"></textarea>
-        <button type="submit">Ask</button>
-    </form>
-    """
-
-# This is the route that the form sends the question to and sends back the response
-@app.route("/test-ai", methods=["POST"])
-def ask_response():
-    # Get the question from the form
-    question = request.form.get("question")
-
-    # Return the response from the AI
-    return get_response(question)
+# Put the extra routes here
 
 
 
 
-@app.get('/ask')
-def ask():
-    # return render_template('hello.html', name=request.args.get('name'))
-  return render_template("ask.html")
 
 
-@app.route('/contextless-message', methods=['GET', 'POST'])
-def contextless_message():
-    question = request.json['message']
-    resp = get_response(question)
-    return {"resp": resp[0]}
-
-
+# This is for when there is not a matching route. 
 @app.errorhandler(404)
 def handle_404(e):
     return '<h1>404</h1><p>File not found!</p><img src="https://httpcats.com/404.jpg" alt="cat in box" width=400>', 404
